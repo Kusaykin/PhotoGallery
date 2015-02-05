95fbe8469e38188d79e0d766120bfa3527eb1412
Kusaykin <susanindin@gmail.com>
2015-02-05 12:47:40 +0600

    Initial commit

---
+++ app/src/main/java/photogallery/android/bignerdranch/com/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,14 @@
+package photogallery.android.bignerdranch.com.photogallery;
+
+import android.support.v4.app.Fragment;
+
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+	@Override
+	public Fragment createFragment() {
+		return new PhotoGalleryFragment();
+	}
+
+}
