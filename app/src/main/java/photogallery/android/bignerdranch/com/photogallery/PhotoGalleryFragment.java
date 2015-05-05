3a6955ea08b66ed1204cff918d0eb18c370512a8
Kusaykin <susanindin@gmail.com>
2015-05-05 17:08:53 +0600

    end of chapter 27

--- app/src/main/java/photogallery/android/bignerdranch/com/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/photogallery/android/bignerdranch/com/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package photogallery.android.bignerdranch.com.photogallery;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,12 +22,12 @@ public class PhotoGalleryFragment extends Fragment {
 	private static final String TAG = "PhotoGalleryFragment";
 	GridView               mGridView;
 	ArrayList<GalleryItem> mItems;
+	ThumbnailDownloader<ImageView> mThumbnailThread;
 
 	void setupAdapter() {
 		if (getActivity() == null || mGridView == null) return;
 		if (mItems != null) {
-			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
-					android.R.layout.simple_gallery_item, mItems));
+			mGridView.setAdapter(new GalleryItemAdapter(mItems));
 		} else {
 			mGridView.setAdapter(null);
 		}
@@ -36,6 +38,10 @@ public class PhotoGalleryFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		setRetainInstance(true);
 		new FetchItemsTask().execute();
+		//mThumbnailThread=new ThumbnailDownladoader<ImageView>();
+		mThumbnailThread.start();
+		mThumbnailThread.getLooper();
+		Log.i(TAG, "Background thread started");
 	}
 
 	@Override
@@ -49,6 +55,12 @@ public class PhotoGalleryFragment extends Fragment {
 
 		return v;
 	}
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		mThumbnailThread.quit();
+		Log.i(TAG, "Background thread destroyed");
+	}
 	private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>> {
 		@Override
 		protected ArrayList<GalleryItem> doInBackground(Void... params)  {
@@ -58,6 +70,24 @@ public class PhotoGalleryFragment extends Fragment {
 		protected void onPostExecute(ArrayList<GalleryItem> items) {
 			mItems = items;
 			setupAdapter();
+		}
+	}
+	private class GalleryItemAdapter extends ArrayAdapter<GalleryItem>{
+
+		public GalleryItemAdapter(ArrayList<GalleryItem> item) {
+			super(getActivity(),0,item);
+		}
+		@Override
+		public  View getView(int position, View convertView, ViewGroup paren) {
+			if (convertView==null){
+				convertView=getActivity().getLayoutInflater().inflate(R.layout.gallery_item, paren, false);
+			}
+			ImageView imageView = (ImageView) convertView.findViewById((R.id.gallery_item_imageView));
+			imageView.setImageResource(R.drawable.cat_low);
+			GalleryItem item=getItem(position);
+			mThumbnailThread.queueThombnail(imageView, item.getUrl());
+
+			return convertView;
 		}
 	}
 }
